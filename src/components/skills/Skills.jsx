f2312e3443755c748836c1727ad324a4058bed00
sidicksino <sidickabdoulayesino1@gmail.com>
2025-09-19 22:01:08 +0200

    Add install motion framer

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./Skills.css";
 import { FaDatabase, FaCode, FaChartBar } from "react-icons/fa";
 import SkillsPhoto from "../../assets/skills.png";
+import { motion } from "framer-motion";
+
 const Skills = () => {
   return (
     <section className="skills" id="skills">
-      <h2 className="skills-title">Skills</h2>
+      <motion.h2
+        initial={{ opacity: 0, translateX: "50%" }}
+        whileInView={{ opacity: 1, translateX: 0 }}
+        transition={{ duration: 1 }}
+        className="skills-title"
+      >
+        Skills
+      </motion.h2>
 
       <div className="skills-container">
         {/* LEFT SIDE */}
@@ -14,39 +23,57 @@ const Skills = () => {
             <div className="icon">
               <FaChartBar />
             </div>
-            <div className="text-blocks">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="text-blocks"
+            >
               <h3>Data Science & Visualization</h3>
               <p>
                 R, Pandas, NumPy, Scikit-learn, TensorFlow, Power BI, Tableau,
                 Matplotlib, Seaborn
               </p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="skill-category">
             <div className="icon">
               <FaCode />
             </div>
-            <div className="text-blocks">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="text-blocks"
+            >
               <h3>Frontend Development</h3>
               <p>HTML, CSS, JavaScript, React.js, React Native</p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="skill-category">
             <div className="icon">
               <FaDatabase />
             </div>
-            <div className="text-blocks">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="text-blocks"
+            >
               <h3>Backend Development</h3>
               <p>Python, Node.js, SQL, MySQL, PostgreSQL, MongoDB</p>
-            </div>
+            </motion.div>
           </div>
         </div>
 
         {/* RIGHT SIDE */}
         <div className="skills-img">
-          <img
+          <motion.img
+            initial={{ opacity: 0, translateX: "50%" }}
+            whileInView={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 1 }}
             src={SkillsPhoto}
             alt="Sidick Abdoulaye Hissein"
             className="skills-img"
