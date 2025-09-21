c9b54a419c588da0479b37112bc404710aed9333
sidicksino <sidickabdoulayesino1@gmail.com>
2025-09-21 16:21:03 +0200

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,12 @@ import WebProjects from "./components/projects/WebProjects";
 import MobileProjects from "./components/projects/MobileProjects";
 import DesignProjects from "./components/projects/DesignProjects";
 import AIProjects from "./components/projects/AIProjects";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route
           path="/"
