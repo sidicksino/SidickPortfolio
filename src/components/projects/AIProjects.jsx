e49cce425e385e4c3fd51be2928291fa06ac06c6
sidicksino <sidickabdoulayesino1@gmail.com>
2025-09-21 18:56:51 +0200

    Modify Projects

--- src/components/projects/AIProjects.jsx
+++ src/components/projects/AIProjects.jsx
@@ -6,26 +6,31 @@ import Insta from "../../assets/insta.png";
 import SinoEcommerce from "../../assets/team-work.png";
 
 const AIProjects = () => {
-  const projects = [   
+  const projects = [
     {
       id: 1,
       title: "Sino Ai",
       description:
         "A modern AI web application, providing intelligent features and real-time interactions.",
-      technologies: ["React", "Node.js", "MongoDB", "OpenAI API", "Tailwind CSS", "Axios"],
+      technologies: [
+        "React",
+        "Node.js",
+        "MongoDB",
+        "OpenAI API",
+        "Tailwind CSS",
+        "Axios",
+      ],
       image: SinoAi,
       liveUrl: "https://sinoai-chi.vercel.app/",
       githubUrl: "https://github.com/sidicksino/sino_Ai",
-    },      
+    },
   ];
 
   return (
     <div className="projects-page">
-      <h1>My Mobile Projects</h1>
-      <p>
-        iOS & Android applications designed for the best user experience.
-      </p>
-  
+      <h1>My AI & ML Projects</h1>
+      <p>Intelligent models for automation, prediction, and optimization.</p>
+
       <div className="projects-list">
         {projects.map((project) => (
           <div key={project.id} className="project-wrapper">
@@ -39,7 +44,7 @@ const AIProjects = () => {
             <div className="project-card1">
               <h3 className="project-title">{project.title}</h3>
               <p className="project-description">{project.description}</p>
-  
+
               <div className="project-tech">
                 {project.technologies.map((tech, index) => (
                   <span key={index} className="tech-tag">
@@ -47,7 +52,7 @@ const AIProjects = () => {
                   </span>
                 ))}
               </div>
-  
+
               <div className="project-links">
                 <a
                   href={project.liveUrl.trim()}
@@ -73,7 +78,7 @@ const AIProjects = () => {
         ))}
       </div>
     </div>
-  );  
+  );
 };
 
 export default AIProjects;
