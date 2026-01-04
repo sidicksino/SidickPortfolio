cad65ebcb912a628aeed9754e27243746953c8b9
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-04 19:59:42 +0200

    feat: Remove `nonOnMobile` class and add new masked content with a title and description.

--- src/components/art/Art.jsx
+++ src/components/art/Art.jsx
@@ -32,7 +32,7 @@ const Art = () => {
 		<h2 className="will-fade ">Sidick Sino</h2>
 		
 		<div className="content">
-		 <ul className="space-y-4 will-fade nonOnMobile">
+		 <ul className="space-y-4 will-fade">
 			{goodLists.map((feature, index) => (
 			 <li key={index} className="flex items-center gap-2">
 				<img src="/images/check.png" alt="check" />
@@ -61,6 +61,10 @@ const Art = () => {
 		
 		<div className="masked-container">
 		 <h2 className="will-fade">Code qui Convertit, Design qui Captive</h2>
+		 <div id="masked-content">
+			<h3>Made with Craft, Poured with Passion</h3>
+			<p>This isn’t just a drink. It’s a carefully crafted moment made just for you.</p>
+		 </div>
 		</div>
 	 </div>
 	</div>
