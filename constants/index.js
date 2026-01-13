f89f6497521508b400a2f4878c44f07a17668fc3
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-13 20:13:49 +0200

    Add same text

--- constants/index.js
+++ constants/index.js
@@ -1,18 +1,4 @@
-// const featureLists = [
-//  "Expertise Full-Stack",
-//  "Optimisation et performance",
-//  "Code propre et documenté",
-//  "Approche Agile",
-// ];
 
-// const goodLists = [
-//  "UX/UI intuitive",
-//  "Design adaptatif (Responsive)",
-//  "CI/CD automatisé",
-//  "Tests rigoureux",
-// ];
-
-// export { featureLists, goodLists };
 
 const featureLists = [
   "Full-Stack",
