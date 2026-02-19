19c253cf2727cbe3c33391f274acb123b3ec32fd
SidickSino <sidickabdoulayesino1@gmail.com>
2026-02-19 07:56:03 +0200

    Fix import path for Art component in App

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Contact from "./components/contact/Contact";
 import ScrollToTop from "./components/ScrollToTop";
 import ThemeToggle from "./components/ThemeToggle";
 import HeroPage from "./components/pages/HeroPage";
-import Art from "../art/Art";
+import Art from "./components/art/Art";
 
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
